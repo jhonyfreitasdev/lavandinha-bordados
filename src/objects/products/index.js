f8b3ac87dc79b71e51d5323629c71626c0066f0f
Jhony Freitas <jhony00._@hotmail.com>
2023-07-26 23:31:21 -0300

    Criando arquivo teste coma lista dev produtos

---
+++ src/objects/products/index.js
@@ -0,0 +1,87 @@
+const chaveirinhos = [
+    {
+        image: '',
+        describe: 'Mini chaveiros de 4cm',
+        price: 'R$ 22,00'
+    },
+    {
+        image: '',
+        describe: 'Mini chaveiros de 4cm',
+        price: 'R$ 22,00'
+    }
+]
+
+const bastidores = [
+    {
+        image: '',
+        describe: 'Mini bastidores de 4cm',
+        price: 'R$ 22,00'
+    },
+    {
+        image: '',
+        describe: 'Mini bastidores de 4cm',
+        price: 'R$ 22,00'
+    }
+]
+
+const ecobag = [
+    {
+        image: '',
+        describe: 'Mini ecobag de 4cm',
+        price: 'R$ 22,00'
+    },
+    {
+        image: '',
+        describe: 'Mini ecobag de 4cm',
+        price: 'R$ 22,00'
+    }
+]
+
+const necessaire = [
+    {
+        image: '',
+        describe: 'Mini necessaire de 4cm',
+        price: 'R$ 22,00'
+    },
+    {
+        image: '',
+        describe: 'Mini necessaire de 4cm',
+        price: 'R$ 22,00'
+    }
+]
+
+const quadros = [
+    {
+        image: '',
+        describe: 'Mini quadros de 4cm',
+        price: 'R$ 22,00'
+    },
+    {
+        image: '',
+        describe: 'Mini ecobag de 4cm',
+        price: 'R$ 22,00'
+    }
+]
+
+export const products = [
+    {
+        id: 'chaveirinhos',
+        products: chaveirinhos
+    },
+    {
+        id: 'bastidores',
+        products: bastidores
+    },
+    {
+        id: 'ecobag',
+        products: ecobag
+    },
+    {
+        id: 'necessaire',
+        products: necessaire
+    },
+    {
+        id: 'quadros',
+        products: quadros
+    }
+]
