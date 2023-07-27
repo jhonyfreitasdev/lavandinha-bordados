ef24db7f6e6491632754e6ca3aa25fed8c2689e3
Jhony Freitas <jhony00._@hotmail.com>
2023-07-27 08:14:05 -0300

    Adicionando icones das redes sociais

--- src/pages/contact/index.jsx
+++ src/pages/contact/index.jsx
@@ -1,5 +1,8 @@
-import { Container, Logo, Title, Text, Item, Image } from "./style.jsx"
-import ImageContact from "../../assets/images/image-contact.png"
+import { Container, Logo, Title, Text, List, Icon, Image } from "./style.jsx";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faWhatsapp, faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-regular-svg-icons';
+import ImageContact from "../../assets/images/image-contact.png";
 
 export const Contact = () => {
     return (
@@ -7,14 +10,29 @@ export const Contact = () => {
             <Logo src="" alt="Imagem do logo" />
 
             <Title>  Contato </Title>
-            <Text>Gostou de algum dos nossos produtos?<br/> Entre em contato com a gente. Será um prazer te atender!</Text>
+            <Text>Gostou de algum dos nossos produtos?<br /> Entre em contato com a gente, será um prazer te atender!</Text>
 
             <address>
-                <ul>
-                    <Item> <a href=""> WhatsApp </a> </Item>
-                    <Item> <a href=""> Instagram </a> </Item>
-                    <Item> <a href=""> E-mail </a> </Item>
-                </ul>
+
+                <List>
+                    <Icon>
+                        <a href="">
+                            <FontAwesomeIcon icon={faWhatsapp} />
+                        </a>
+                    </Icon>
+
+                    <Icon>
+                        <a href="">
+                            <FontAwesomeIcon icon={faInstagram} />
+                        </a>
+                    </Icon>
+
+                    <Icon>
+                        <a href="">
+                            <FontAwesomeIcon icon={faEnvelope} />
+                        </a>
+                    </Icon>
+                </List>
             </address>
 
             <Image src={ImageContact} alt="Imagem animada da criadora" />
--- src/pages/contact/style.jsx
+++ src/pages/contact/style.jsx
@@ -1,12 +1,13 @@
 import { styled } from "styled-components";
 import "../../assets/styles/fonts.css"
 
-const Container = styled.div `
+const Container = styled.div`
     position: relative;
     display: flex;
     flex-direction: column;
     justify-content: flex-start;
     align-items: center;
+    gap: 12px;
     background-color: #D5C3DD;
     font-family: 'Hatton';
     margin: 0 auto;
@@ -15,17 +16,17 @@ const Container = styled.div `
     max-width: 1440px;
 `
 
-const Logo = styled.img `
+const Logo = styled.img`
 
 `
 
-const Title = styled.h2 `
+const Title = styled.h2`
     color: #756A8B;
     font-size: 35px;
     font-weight: 600;
 `
 
-const Text = styled.p `
+const Text = styled.p`
     color: #FFFFFF;
     font-size: 23px;
     font-weight: 600;
@@ -33,25 +34,24 @@ const Text = styled.p `
     width: 40%;
 `
 
-const Item = styled.li `
-    background: linear-gradient(to right, #756A8B, #BEA7C8);
-    text-align: center;
-    border-radius: 35px;
-    box-shadow: 1px 0 25px 2px #00000014;
-    width: 170px;
-    padding: 9px;
-    margin: 12px 0;
-    
-    a {
-        color: #FFFFFF;
-        font-size: 22px;
-        font-weight: 600;
+const List = styled.ul`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 25px;
+`
+
+const Icon = styled.li`
+    font-size: 35px;
+    a{
+        color: #756A8B;
     }
 `
 
-const Image = styled.img `
+const Image = styled.img`
     position: absolute;
-    top: 23%;
+    top: 9%;
     right: 30px;
+    width: 280px;
 `
-export { Container, Logo, Title, Text, Item, Image }
+export { Container, Logo, Title, Text, List, Icon, Image }
