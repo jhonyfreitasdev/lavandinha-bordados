490064c4a89e7c7d64b4ce96b91eede2810f45ba
Jhony Freitas <jhony00._@hotmail.com>
2023-07-27 23:30:22 -0300

    Fazendo condicional das listas para renderização

--- src/pages/products/index.jsx
+++ src/pages/products/index.jsx
@@ -1,9 +1,16 @@
+import { useContext } from "react"
+import { FilterContext } from "../../context/filter-products"
+import { SelectButton } from "../../components/select-product-button"
 import { ProductList } from "../../components/product-list"
+import { FilteredList } from "../../components/filtered-list"
 
 export const Products = () => {
+    const { filteredProductName } = useContext(FilterContext)
+ 
     return (
         <>
-            <ProductList />
+            <SelectButton />
+            {filteredProductName === '' ? <ProductList /> : <FilteredList />}
         </>
     )
 }
