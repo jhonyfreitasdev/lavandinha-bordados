78c7e7394cb7183a75f2971a6711690916c8b69c
Jhony Freitas <jhony00._@hotmail.com>
2023-08-02 07:59:00 -0300

    Atualizando lista de produtos

--- src/objects/products/index.js
+++ src/objects/products/index.js
@@ -2,86 +2,100 @@ import { images } from "../images"
 
 const chaveirinhos = [
     {
+        id: 'chaveirinhos 1',
         image: images.chaveiroPet,
         describe: 'Chaveiro pet personalizado',
-        price: 'R$ 22,00'
+        price: 22.00
     },
     {
+        id: 'chaveirinhos 2',
         image: images.chaveiroPet2,
         describe: 'Chaveiro pet personalizado',
-        price: 'R$ 22,00'
+        price: 22.00
     },
     {
+        id: 'chaveirinhos 3',
         image: images.chaveiroMickey,
         describe: 'Chaveiro Mickey',
-        price: 'R$ 22,00'
+        price: 22.00
     },
     {
+        id: 'chaveirinhos 4',
         image: images.chaveiroNSAparecida,
         describe: 'Chaveiro N.S. Aparecida',
-        price: 'R$ 22,00'
+        price: 22.00
     },
     {
+        id: 'chaveirinhos 5',
         image: images.chaveiroAbelha,
         describe: 'Chaveiro Abelhinha',
-        price: 'R$ 22,00'
+        price: 22.00
     },
 ]
 
 const bastidores = [
     {
+        id: 'bastidor 1',
         image: images.bastidorVenus,
         describe: 'Bastidor Vênus - 16cm',
-        price: 'R$ 54,00'
+        price: 54.00
     },
     {
+        id: 'bastidor 2',
         image: images.bastidorCoelha,
         describe: 'Bastidor Coelhinha - 16cm',
-        price: 'R$ 54,00'
+        price: 54.00
     }
 ]
 
 const ecobag = [
     {
+        id: 'ecobag 1',
         image: images.ecobagLibelula,
         describe: 'Ecobag Libélula',
-        price: 'R$ 56,00'
+        price: 56.00
     },
     {
+        id: 'ecobag 2',
         image: images.ecobabFloresAlgodao,
         describe: 'Ecobag Flores de Algodão',
-        price: 'R$ 56,00'
+        price: 56.00
     },
     {
+        id: 'ecobag 3',
         image: images.ecobagGatos,
         describe: 'Ecobag Gato e Arte',
-        price: 'R$ 56,00'
+        price: 56.00
     },
     {
+        id: 'ecobag 4',
         image: images.ecobagFloresAzuis,
         describe: 'Ecobag Flores Azuis',
-        price: 'R$ 56,00'
+        price: 56.00
     },
 ]
 
 const camisetas = [
     {
+        id: 'camiseta 1',
         image: images.camisetaSaoJose,
         describe: 'Camiseta São José',
-        price: 'R$ 100,00'
+        price: 100.00
     },
     {
+        id: 'camiseta 2',
         image: images.camisetaPersonalizada,
         describe: 'Camiseta com bordado personalizado',
-        price: 'R$ 100,00'
+        price: 100.00
     }
 ]
 
 const quadros = [
     {
+        id: 'quadro 1',
         image: images.quadroPersonalizado,
         describe: 'Quadro com bordado personalizado',
-        price: 'R$ 100,00'
+        price: 100.00
     }
 ]
 
