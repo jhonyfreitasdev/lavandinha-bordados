c594c49f3ba7cecd2cd38fa1f23ace05578bfd6b
Jhony Freitas <jhony00._@hotmail.com>
2023-08-03 23:27:25 -0300

    Adicionando medidas do responsivo

---
+++ src/objects/device.js
@@ -0,0 +1,13 @@
+const size = {
+    mobileM: '385px',
+    mobileL: '450px',
+    tablet: '665px',
+    laptop: '1015px',
+}
+
+export const device = {
+    mobileM: `(max-width: ${size.mobileM})`,
+    mobileL: `(max-width: ${size.mobileL})`,
+    tablet: `(max-width: ${size.tablet})`,
+    laptop: `(max-width: ${size.laptop})`,
+};
